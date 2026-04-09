7f4b38ae827955191a891a0e449a44edae7b3b14
comradescallion <schmoogalooga@gmail.com>
2026-04-09 19:02:59 -0400

    take path

--- checkers-but-awesome/objects/tile.gd
+++ checkers-but-awesome/objects/tile.gd
@@ -4,7 +4,7 @@ class_name Tile extends Sprite2D
 #region Textures
 const ICON = preload("uid://dgiqw4yimxh3") #placeholder
 const BLACK = preload("uid://brnwttvfed8oy")
-const RED = preload("uid://dfb2xgfoofrw")
+const TEAL = preload("uid://dfb2xgfoofrw")
 const WHITE = preload("uid://ykkd0vn8m4rr")
 #endregion
 
@@ -19,9 +19,13 @@ enum TYPE {
 		type = value
 		if is_inside_tree(): update_values()
 
+@export var highlighted := false
+
 var y: int
 var x: int
 var size: int
+
+@export_tool_button("Get Valid Moves") var moves_action = get_possible_moves
 
 @export_group("Link References")
 @export var northwest: Tile = null
@@ -49,6 +53,15 @@ func load_name() -> void:
 	name = "Tile(" + str(y) + "," + str(x) + ")"
 
 func _process(_delta: float) -> void:
+	if highlighted:
+		if modulate == Color.WHITE:
+			var tween = create_tween()
+			tween.tween_property(self, "modulate", Color(0.992, 1.0, 0.427, 1.0), 0.25)
+		elif modulate == Color(0.992, 1.0, 0.427, 1.0):
+			var tween = create_tween()
+			tween.tween_property(self, "modulate", Color.WHITE, 0.25)
+	else:
+		modulate = Color.WHITE
 	pass
 
 func update_values() -> void:
@@ -57,7 +70,7 @@ func update_values() -> void:
 	match type:
 		TYPE.EMPTY: texture = WHITE
 		TYPE.FILLED: texture = BLACK
-		TYPE.NULL: texture = RED
+		TYPE.NULL: texture = TEAL
 		_: texture = ICON
 		
 	# Moving scale and position calculations here so they update dynamically 
@@ -70,3 +83,79 @@ func update_values() -> void:
 		
 func _to_string() -> String:
 	return "(" + str(y) + "," + str(x) + "): " + str(type)
+	
+func get_possible_moves() -> Array[TakePath]:
+	#var surrounding: Array[Tile] = [northwest, north, northeast, west, east, southwest, south, southeast]
+	# lookup for possible move, and the piece it would take ([new position, taken piece])
+	var possible_moves: Array[TakePath] = []
+	
+	# there has to be a better way to do this
+	# oh well #myowngrave
+	
+	# UP+LEFT
+	if is_instance_valid(northwest) and northwest.type == TYPE.FILLED:
+		#check for next piece
+		if is_instance_valid(northwest.northwest) and northwest.northwest.type == TYPE.EMPTY:
+			possible_moves.append(TakePath.new(self, northwest.northwest, northwest))
+		#bounce off the wall possibility
+		if (not is_instance_valid(northwest.northwest) or northwest.northwest.type == TYPE.NULL) and (not is_instance_valid(northwest.west) or northwest.west.type == TYPE.NULL):
+			if is_instance_valid(northwest.northeast) and northwest.northeast.type == TYPE.EMPTY:
+				possible_moves.append(TakePath.new(self, northwest.northeast, northwest))
+					
+	# UP
+	if is_instance_valid(north) and north.type == TYPE.FILLED:
+		if is_instance_valid(north.north) and north.north.type == TYPE.EMPTY:
+			possible_moves.append(TakePath.new(self, north.north, north))
+			
+	# UP+RIGHT
+	if is_instance_valid(northeast) and northeast.type == TYPE.FILLED:
+		#check for next piece
+		if is_instance_valid(northeast.northeast) and northeast.northeast.type == TYPE.EMPTY:
+			possible_moves.append(TakePath.new(self, northeast.northeast, northeast))
+		#bounce off the wall possibility
+		if (not is_instance_valid(northeast.northeast) or northeast.northeast.type == TYPE.NULL) and (not is_instance_valid(northeast.east) or northeast.east.type == TYPE.NULL):
+			if is_instance_valid(northeast.northwest) and northeast.northwest.type == TYPE.EMPTY:
+				possible_moves.append(TakePath.new(self, northeast.northwest, northeast))
+	
+	# LEFT
+	if is_instance_valid(west) and west.type == TYPE.FILLED:
+		if is_instance_valid(west.west) and west.west.type == TYPE.EMPTY:
+			possible_moves.append(TakePath.new(self, west.west, west))
+			
+	# RIGHT
+	if is_instance_valid(east) and east.type == TYPE.FILLED:
+		if is_instance_valid(east.east) and east.east.type == TYPE.EMPTY:
+			possible_moves.append(TakePath.new(self, east.east, east))
+			
+	# DOWN+LEFT
+	if is_instance_valid(southwest) and southwest.type == TYPE.FILLED:
+		#check for next piece
+		if is_instance_valid(southwest.southwest) and southwest.southwest.type == TYPE.EMPTY:
+			possible_moves.append(TakePath.new(self, southwest.southwest, southwest))
+		#bounce off the wall possibility
+		if (not is_instance_valid(southwest.southwest) or southwest.southwest.type == TYPE.NULL) and (not is_instance_valid(southwest.west) or southwest.west.type == TYPE.NULL):
+			if is_instance_valid(southwest.southeast) and southwest.southeast.type == TYPE.EMPTY:
+				possible_moves.append(TakePath.new(self, southwest.southeast, southwest))
+					
+	# DOWN
+	if is_instance_valid(south) and south.type == TYPE.FILLED:
+		if is_instance_valid(south.south) and south.south.type == TYPE.EMPTY:
+			possible_moves.append(TakePath.new(self, south.south, south))
+			
+	# DOWN+RIGHT
+	if is_instance_valid(southeast) and southeast.type == TYPE.FILLED:
+		#check for next piece
+		if is_instance_valid(southeast.southeast) and southeast.southeast.type == TYPE.EMPTY:
+			possible_moves.append(TakePath.new(self, southeast.southeast, southeast))
+		#bounce off the wall possibility
+		if (not is_instance_valid(southeast.southeast) or southeast.southeast.type == TYPE.NULL) and (not is_instance_valid(southeast.east) or southeast.east.type == TYPE.NULL):
+			if is_instance_valid(southeast.southwest) and southeast.southwest.type == TYPE.EMPTY:
+				possible_moves.append(TakePath.new(self, southeast.southwest, southeast))
+	
+	
+	print(possible_moves)
+	
+	for p: TakePath in possible_moves:
+		p.show_path()
+	
+	return possible_moves
