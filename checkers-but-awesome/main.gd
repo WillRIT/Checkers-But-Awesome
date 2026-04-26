06fed919ffeba4f9cdc6fc68d206940027e89d35
gunnardickey1 <56365361+gunnardickey1@users.noreply.github.com>
2026-04-26 16:13:30 -0400

    finished levels and fixed camera

--- checkers-but-awesome/main.gd
+++ checkers-but-awesome/main.gd
@@ -6,41 +6,62 @@ enum GAMESTATE{
 	WIN,
 	LOSE
 }
-var levels = [ "OOOOO
+var levels = [
+"#OO
+OXO
+OOO",
+"  #OO
+  OXO
+OOOOO
+OXOXO
+OOOOO",
+"#XOXO
+OOOOX
+OXOXO",
+"OOOOO
 OXOXO
 OOOOO
 XOOOX
 OX#XO",
 "OOOXOOO
+OXOOOXO
+OOOO#OO
+XOOXOXO
+OXOOOOO",
+"OOOXOOO
 OXOOXOO
 OXOOOOO
 OOX XXO
 OO# OOO",
-"  OOO  
-  OXX  
-OXO OO#
-OX   XX
-OOO OOO
-  OXO  
-  OXO  ",
 "OXOOOOOXO
 OOOXOXOXO
 ..OOOOO..
 OXOXOOOXO
 OX#OOXOXO",
-"  OOO
-  OXX
-OX#XO
-XXO  
-OOO  "
+"OO#XO
+XXOXO
+OXOXO",
+"OOOOOXOXO
+OXXXOXOXO
+OOOO#OOOO
+XXOXX    
+OOOOO    ",
+"     OOOO
+     OXXO
+OX#XOOOOO
+OXOXOXOXO
+OOOXOXOOO"
 ]
 @export var level : int
 @export var state : GAMESTATE
 @export var start_scene: PackedScene
+
 var board : Node
 var pieces : Array
 var player : Node
+var boardoffset : int
 func _ready() -> void:
+	
 	board = find_child("Board");
 
 func _process(delta: float) -> void:
@@ -58,6 +79,14 @@ func state_change(newstate: GAMESTATE) -> void:
 		GAMESTATE.GAME:
 			board.loadable = levels[level-1]
 			board.load_string()
+			if(board.width < 9):
+				board.position.x = 380.0
+				board.find_child("Reset").position.x = 0
+				board.position.x += (9 - board.width)/2 * 50
+				board.find_child("Reset").position.x -= (9 - board.width)/2 * 50
+			else:
+				board.position.x = 380.0
+				board.find_child("Reset").position.x = 0
 			board.show()
 			find_child("Level Selection").hide();
 			find_child("Next Level").hide();
@@ -72,11 +101,20 @@ func state_change(newstate: GAMESTATE) -> void:
 		GAMESTATE.WIN:
 			board.hide()
 			find_child("Next Level").show();
+			if(level == 10):
+				find_child("Next Level").find_child("Button").hide()
+			else:
+				find_child("Next Level").find_child("Button").show()
+			
 			state = newstate
 		GAMESTATE.LOSE:
 			#Lose Screen is shown
 			state = newstate
-
+func change_scene(scene: PackedScene):
+	if scene:
+		get_tree().change_scene_to_packed(scene)
+	else:
+		push_error("Scene is not assigned!")
 
 func _on_button_pressed(selected : int) -> void:
 	level = selected;
@@ -92,4 +130,4 @@ func _back_to_select() -> void:
 	state_change(GAMESTATE.LEVELSELECT)
 
 func _on_menu_pressed() -> void:
-	get_tree().change_scene_to_packed(start_scene)
+	change_scene(start_scene)
